677f2451eb752fbbd620cf73c4d758bf567470da
Joao Pedro Sodre <jpasodre@hotmail.com>
2020-08-31 20:34:22 -0300

    .

---
+++ Assets/TriggerFala.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFala : MonoBehaviour
+{
+    public AI ai;
+    public GameObject barracaBussola;
+    private float timer;
+    private bool falainicial;
+
+    private void Start()
+    {
+        falainicial = false;
+        timer = 15;
+    }
+
+    private void Update()
+    {
+        timer -= Time.timeScale;
+
+        if(timer <= 0 && !falainicial)
+        {
+            ai.SetFalaAtiva(true);
+            falainicial = true;
+            barracaBussola.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/TriggerPlayerEnter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlayerEnter : MonoBehaviour
+{
+    public AI ai;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Veículo"))
+        {
+            ai.SetFalaAtiva(true);
+            Destroy(gameObject);
+        }
+    }
+}
+
